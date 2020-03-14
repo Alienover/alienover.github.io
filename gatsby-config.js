f23ebd2522c4c88f5056a710a7def0de7a0db653
jiarong <jiarong@edison.tech>
2020-03-14 21:10:57 +0800

    Setup git hook and flow in pre-commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,4 +6,13 @@
 
 module.exports = {
   /* Your site config here */
+  plugins: [
+    ['gatsby-plugin-flow'],
+    {
+      resolve: 'gatsby-plugin-google-fonts',
+      options: {
+        fonts: ['material icons', 'roboto:300,400,500,700'],
+      },
+    },
+  ],
 }
